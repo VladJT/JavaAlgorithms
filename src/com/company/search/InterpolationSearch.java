c51e547626ea9aecf5e7fd89d55687c1c8520722
Владислав Богданков <onlyrus@hotmail.com>
2022-01-21 09:37:24 +0300

    dz 3

---
+++ src/com/company/search/InterpolationSearch.java
@@ -0,0 +1,29 @@
+package com.company.search;
+
+public class InterpolationSearch {
+
+    public static int search(int[] arr, int num2Find) {
+        int start = 0;
+        int end = arr.length - 1;
+        int base;
+        int i = 0;
+        while (end >= start && num2Find >= arr[start] && num2Find <= arr[end]) {
+            if (arr[start] == arr[end]) break;
+            i++;
+            base = (int) (start + 1.0 * (end - start) / (arr[end] - arr[start]) * (num2Find - arr[start]));
+            if (arr[base] == num2Find) {
+                System.out.println("Найдено. Итераций: " + i);
+                return base;
+            } else if (arr[base] < num2Find) {
+                start = base + 1;
+            } else {
+                end = base - 1;
+            }
+
+        }
+
+        if (arr[start] == num2Find) return start;
+        System.out.println("НЕ найдено. Итераций: " + i);
+        return -1;
+    }
+}
