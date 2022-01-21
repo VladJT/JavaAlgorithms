43be6b686a13ee55893a4a0b7bac80380aec05c2
Богданков Владислав <onlyrus@hotmail.com>
2022-01-21 22:10:45 +0300

    dz3

---
+++ src/main/java/com/company/core/Lesson1.java
@@ -0,0 +1,12 @@
+package com.company.core;
+
+public class Lesson1 {
+
+    public static void main(String[] args) {
+        double d = 45.7;
+        float f = 5.3f;
+        d += f;
+
+        System.out.println(d);
+    }
+}
