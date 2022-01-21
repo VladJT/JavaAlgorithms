835849d62de59190abaf3bb85c1aa88bfdf9d5fb
Богданков Владислав <onlyrus@hotmail.com>
2022-01-21 22:44:27 +0300

    dz3

--- src/main/java/com/company/core/Lesson1.java
+++ src/main/java/com/company/core/Lesson1.java
@@ -4,9 +4,11 @@ public class Lesson1 {
 
     public static void main(String[] args) {
         double d = 45.7;
-        float f = 5.3f;
-        d += f;
 
+        // utf-8
+        int a = 2;
+        int b = 3;
+        d = 1.0 * a / b;
         System.out.println(d);
     }
 }
