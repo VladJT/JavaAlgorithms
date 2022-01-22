aa298f355266e0ca685aa6c1b7d711460ff34b40
Богданков Владислав <onlyrus@hotmail.com>
2022-01-22 11:37:31 +0300

    dz3

--- src/main/java/com/company/core/Lesson1.java
+++ src/main/java/com/company/core/Lesson1.java
@@ -3,12 +3,47 @@ package com.company.core;
 public class Lesson1 {
 
     public static void main(String[] args) {
-        double d = 45.7;
-
-        // utf-8
-        int a = 2;
-        int b = 3;
-        d = 1.0 * a / b;
-        System.out.println(d);
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
     }
+
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+
+    public static void checkSumSign() {
+        int a = 10;
+        int b = -2;
+        int sum = a + b;
+
+        if (sum >= 0) System.out.println("Сумма положительная");
+        else System.out.println("Сумма отрицательная");
+    }
+
+
+    public static void printColor() {
+        int value = 101;
+        if (value <= 0)
+            System.out.println("Красный");
+        else if (value > 0 && value <= 100)
+            System.out.println("Желтый");
+        else System.out.println("Зеленый");
+    }
+
+
+    public static void compareNumbers() {
+        int a = 10;
+        int b = 8;
+
+        if (a >= b) System.out.println("a >= b");
+        else System.out.println("a < b");
+    }
+
 }
+
