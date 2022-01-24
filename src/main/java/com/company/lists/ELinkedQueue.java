cfefc4df44f86f74b5eccc83f59f461ad093521b
Богданков Владислав <onlyrus@hotmail.com>
2022-01-24 21:44:50 +0300

    linked lists

---
+++ src/main/java/com/company/lists/ELinkedQueue.java
@@ -0,0 +1,51 @@
+package com.company.lists;
+
+public class ELinkedQueue<E> {
+    private final ETwoSideLinkedList<E> data;
+
+    public ELinkedQueue() {
+        data = new ETwoSideLinkedList<E>();
+    }
+
+    public boolean insert(E value) {
+        data.insertLast(value);
+        return true;
+    }
+
+    public E remove() {
+        return data.removeFirst();
+    }
+
+    public E peekFront() {
+        return data.getFirst();
+    }
+
+    public int size() {
+        return data.size();
+    }
+
+    public boolean isEmpty() {
+        return data.isEmpty();
+    }
+
+    public void display() {
+        data.display();
+    }
+
+
+    public static void main(String[] args) {
+        ELinkedQueue<Integer> q = new ELinkedQueue<>();
+        q.insert(1);
+        q.insert(2);
+        q.insert(3);
+        q.insert(4);
+        q.insert(5);
+
+
+        System.out.println("Front: " + q.peekFront());
+        q.remove();
+        q.display();
+
+    }
+
+}
---
+++ src/main/java/com/company/lists/ETwoSideLinkedList.java
@@ -0,0 +1,84 @@
+package com.company.lists;
+
+public class ETwoSideLinkedList<E> extends ELinkedList<E> {
+
+    protected Node<E> last;
+
+    public void insertFirst(E value) {
+        super.insertFirst(value);
+        if (size == 1) {
+            last = first;
+        }
+    }
+
+    public void insertLast(E value) {
+        if (isEmpty()) insertFirst(value);
+        else {
+            Node<E> newNode = new Node<>(value, null);
+            last.next = newNode;
+            last = newNode;
+            size++;
+        }
+    }
+
+    @Override
+    public E removeFirst() {
+        E remValue = super.removeFirst();
+        if (isEmpty()) {
+            last = null;
+        }
+        return remValue;
+    }
+
+    @Override
+    public boolean remove(E value) {
+        Node<E> current = first;
+        Node<E> prev = null;
+
+        while (current != null) {
+            if (current.item.equals(value)) break;
+            prev = current;
+            current = current.next;
+        }
+
+        if (current == null) {
+            return false;
+        } else {
+            if (current == first) {
+                removeFirst();
+                return true;
+            }
+            if(current==last){
+                last = prev;
+                last.next=null;
+            }
+            prev.next = current.next;
+            current.next = null;
+            size--;
+        }
+
+        return true;
+    }
+
+    public E getLast() {
+        return last.item;
+    }
+
+    public static void main(String[] args) {
+        ETwoSideLinkedList<Integer> l = new ETwoSideLinkedList<>();
+        l.insertFirst(5);
+        l.insertFirst(4);
+        l.insertFirst(3);
+        l.insertFirst(2);
+        l.insertFirst(1);
+
+        l.insertLast(8);
+        l.remove(3);
+
+
+        System.out.println("contains 2? - "+l.contains(2));
+        System.out.println("contains 3? - "+l.contains(3));
+        l.display();
+    }
+
+}
