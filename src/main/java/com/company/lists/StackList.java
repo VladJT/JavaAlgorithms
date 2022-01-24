6205f7706d9db6c997191ea458015fe0e2b1b33c
Владислав Богданков <onlyrus@hotmail.com>
2022-01-24 10:50:06 +0300

    dz 3

---
+++ src/main/java/com/company/lists/StackList.java
@@ -0,0 +1,38 @@
+package com.company.lists;
+
+// Реализация стека на базе связного списка
+public class StackList {
+    private LinkedList list;
+
+    public StackList() {
+        list = new LinkedList();
+    }
+
+    public void push(String name, int age) {
+        list.insert(name, age);
+    }
+
+    public String pop() {
+        return list.delete().name;
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public void display() {
+        list.display();
+    }
+
+    public static void main(String[] args) {
+        StackList sl = new StackList();
+        sl.push("Artem", 30);
+        sl.push("Viktor", 20);
+        sl.push("Sergey", 10);
+        sl.display();
+        while (!sl.isEmpty()) {
+            System.out.println("Элемент " + sl.pop() + " удален из стека");
+        }
+    }
+
+}
