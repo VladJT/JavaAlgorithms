f350d2fc8be257fdf8d95ff129e9df32038d10b5
Владислав Богданков <onlyrus@hotmail.com>
2022-01-26 15:41:50 +0300

    домашнее задание №2 (java core)

---
+++ src/main/java/com/company/core/l3_GuessNumber.java
@@ -0,0 +1,43 @@
+package com.company.core;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Написать программу, которая загадывает случайное число от 0 до 9 и пользователю дается 3 попытки угадать это число.
+ * При каждой попытке компьютер должен сообщить, больше ли указанное пользователем число, чем загаданное, или меньше.
+ * После победы или проигрыша выводится запрос – «Повторить игру еще раз? 1 – да / 0 – нет»(1 – повторить, 0 – нет).
+ */
+public class l3_GuessNumber {
+
+    public static void main(String[] args) {
+        int playGame = 1;
+        do {
+            int tryCount = 3;
+            int xNumber = new Random().nextInt(10);
+            boolean isWin = false;
+
+            System.out.printf(xNumber + "Компьютер загадал число от 0 до 9. Попробуйте его угадать %d за попытки%n", tryCount);
+            do {
+                isWin = checkAnswer(xNumber);
+                tryCount--;
+            } while (tryCount > 0 && isWin == false);
+
+            if (isWin)
+                System.out.println("Поздравляем🎈🎈🎈  Вы победили!Оставалось попыток: " + tryCount);
+            else System.out.println("Вы проиграли! Компьютер загадал число " + xNumber);
+
+            System.out.println("Повторить игру еще раз? 1 – да / 0 – нет");
+            playGame = new Scanner(System.in).nextInt();
+        } while (playGame == 1);
+    }
+
+    public static boolean checkAnswer(int xNumber) {
+        System.out.print("Введите число: ");
+        int answer = new Scanner(System.in).nextInt();
+        if (answer > xNumber) System.out.println("Загаданное число меньше");
+        if (answer < xNumber) System.out.println("Загаданное число больше");
+        return answer == xNumber;
+    }
+
+}
---
+++ src/main/java/com/company/core/l3_GuessWord.java
@@ -0,0 +1,61 @@
+package com.company.core;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * При запуске программы компьютер загадывает слово, запрашивает ответ у пользователя, сравнивает его с загаданным словом
+ * и сообщает, правильно ли ответил пользователь.
+ * Если слово не угадано, компьютер показывает буквы, которые стоят на своих местах.
+ * apple – загаданное
+ * apricot - ответ игрока
+ * ap############# (15 символов, чтобы пользователь не мог узнать длину слова)
+ * Для сравнения двух слов посимвольно можно пользоваться:
+ * String str = "apple";
+ * char a = str.charAt(0); - метод, вернет char, который стоит в слове str на первой позиции
+ * Играем до тех пор, пока игрок не отгадает слово.
+ * Используем только маленькие буквы.
+ */
+public class l3_GuessWord {
+
+    public static void main(String[] args) {
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+
+        System.out.println("Задан список слов, угадайте случайно загаданное слово 🎭:");
+        System.out.println(Arrays.toString(words));
+
+        int searchIndex = new Random().nextInt(words.length);
+        String userAnswer = "";
+        boolean isCorrectAnswer = false;
+
+        //TODO delete
+        System.out.println("Компьютер загадал " + words[searchIndex]);
+
+        while (!isCorrectAnswer) {
+            System.out.print("Введите ваш вариант: ");
+            userAnswer = new Scanner(System.in).next();
+
+            isCorrectAnswer = userAnswer.equals(words[searchIndex]);
+            if (!isCorrectAnswer) {
+                showHint(userAnswer, words[searchIndex]);
+            }
+        }
+
+        System.out.println("Вы угадали! Компьютер загадал " + words[searchIndex]);
+    }
+
+    // показ посказки
+    public static void showHint(String userAnswer, String correctAnswer) {
+        String result = "";
+        for (int i = 0; i < 15; i++) {
+            if (userAnswer.length() > i && correctAnswer.length() > i) {
+                if (userAnswer.charAt(i) == correctAnswer.charAt(i))
+                    result += userAnswer.charAt(i);
+                else result += "#";
+            } else result += "#";
+        }
+        System.out.println(result);
+    }
+
+}
