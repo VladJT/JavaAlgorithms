fe34286b424cc3fd08c4647b6d47af398984cff0
Богданков Владислав <onlyrus@hotmail.com>
2022-02-07 19:48:06 +0300

    trees

--- src/main/java/com/company/CodeWars.java
+++ src/main/java/com/company/CodeWars.java
@@ -5,24 +5,16 @@ import java.util.*;
 
 public class CodeWars {
 
+    public static String printerError(String s) {
+        return s.replaceAll("[a-m]", "");
 
-    public static int countPassengers(ArrayList<int[]> stops) {
-        //Code here!
-        int count = 0;
-        for (int[] a : stops) {
-            count = count + a[0] - a[1];
-        }
-        return count;
     }
 
 
     public static void main(String[] args) {
-        ArrayList<int[]> list = new ArrayList<>();
-        list.add(new int[]{10, 0});
-        list.add(new int[]{3, 5});
-        list.add(new int[]{2, 5});
-        System.out.println(countPassengers(list));
 
+        String s="aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz";
+        System.out.println(printerError(s));
 
     }
 }
