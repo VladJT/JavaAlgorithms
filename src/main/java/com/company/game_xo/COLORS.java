d9f257828432f6d162e48fb4786fe7391fe13bbb
Богданков Владислав <onlyrus@hotmail.com>
2022-02-08 22:42:12 +0300

    graphs

---
+++ src/main/java/com/company/game_xo/COLORS.java
@@ -0,0 +1,11 @@
+package com.company.game_xo;
+
+public class COLORS {
+    public static final String SYS = "\u001b[0m";
+    public static final String GREY = "\u001b[90m";
+    public static final String RED = "\u001b[31m";
+    public static final String GREEN = "\u001b[32m";
+    public static final String YELLOW = "\u001b[33m";
+    public static final String BLUE = "\u001b[34m";
+    public static final String MENU = "\u001b[100m";
+}
--- src/main/java/com/company/game_xo/ConsoleGameXO.java
+++ src/main/java/com/company/game_xo/ConsoleGameXO.java
@@ -1,15 +1,19 @@
 package com.company.game_xo;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class ConsoleGameXO {
 
     private static int SIZE = 3;
+    private static int WIN_SIZE = 3;
+
     private static char[][] map;
 
-
-    private static final char DOT_EMPTY = '.';
+    private static final char DOT_EMPTY = '_';
     private static final char DOT_X = 'X';
     private static final char DOT_O = 'O';
 
@@ -17,23 +21,41 @@ public class ConsoleGameXO {
     private static Random random = new Random();
 
 
-    public static void main(String[] args) {
-        initMap();
-        printMap();
-        while (true) {
-            humanTurn();
-            if (isEndGame(DOT_X)) {
-                break;
-            }
+    public static void main(String[] args) throws InterruptedException {
+        do {
+//            System.out.print("Введите размер поля (3-20): ");
+//            SIZE = scanner.nextInt();
+            SIZE = 5;
+            if (SIZE >= 6 || SIZE <= 10) WIN_SIZE = 4;
+            if (SIZE > 10) WIN_SIZE = 5;
 
-            aiTurn();
-            if (isEndGame(DOT_O)) {
-                break;
-            }
+            initMap();
+            printMap();
+            while (true) {
+                humanTurn();
+                if (isEndGame(DOT_X)) {
+                    break;
+                }
 
-        }
+                TimeUnit.MILLISECONDS.sleep(800);
+
+                aiTurn();
+                if (isEndGame(DOT_O)) {
+                    break;
+                }
+
+            }
+        } while (isContinueGame());
         System.out.println("Игра окончена");
     }
+
+    private static boolean isContinueGame() {
+        System.out.println("Сыграем еще? 1-да, 0-нет");
+        scanner.nextLine();
+        String s = scanner.nextLine().toLowerCase();
+        return s.equals("1") || s.equals("да") || s.equals("y");
+    }
+
 
     private static void initMap() {
         map = new char[SIZE][SIZE];
@@ -43,11 +65,7 @@ public class ConsoleGameXO {
     }
 
     private static void printMap() {
-        // линии координат
-        for (int i = 0; i <= SIZE; i++)
-            System.out.print(i + " ");
-        System.out.println();
-
+        printTopNumLine();// линии координат
         // выводим map
         for (int i = 0; i < SIZE; i++) {
             System.out.print((i + 1) + " ");
@@ -59,8 +77,27 @@ public class ConsoleGameXO {
         System.out.println();
     }
 
+    private static void printTopNumLine() {
+        for (int i = 0; i <= SIZE; i++)
+            System.out.print(i + " ");
+        System.out.println();
+    }
+
+    private static void printMap(int x, int y, String color) {
+        printTopNumLine();// линии координат
+        // выводим map
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < SIZE; j++) {
+                if (i == x && y == j) System.out.print(color + map[i][j] + COLORS.SYS + " ");
+                else System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
     private static boolean isEndGame(char playerSymbol) {
-        printMap();
         if (checkWin(playerSymbol)) {
             System.out.println("Победили " + playerSymbol);
             return true;
@@ -73,13 +110,13 @@ public class ConsoleGameXO {
     }
 
     private static boolean checkWin(char playerSymbol) {
-        if (checkWinDiagonals(playerSymbol) || checkWinLines(playerSymbol)) {
+        if (checkWinAllDiagonals(playerSymbol) || checkWinAllLines(playerSymbol)) {
             return true;
         }
         return false;
     }
 
-    private static boolean checkWinLines(char playerSymbol) {
+    private static boolean checkWinAllLines(char playerSymbol) {
         boolean cols, rows;
 
         for (int col = 0; col < SIZE; col++) {
@@ -94,7 +131,7 @@ public class ConsoleGameXO {
         return false;
     }
 
-    private static boolean checkWinDiagonals(char playerSymbol) {
+    private static boolean checkWinAllDiagonals(char playerSymbol) {
         boolean leftRight, rightLeft, result;
         leftRight = true;
         rightLeft = true;
@@ -129,14 +166,42 @@ public class ConsoleGameXO {
     }
 
 
+//    private static void humanTurn() {
+//        int x, y;
+//        do {
+//            System.out.print("Введите координаты свободной ячейки: ");
+//            x = scanner.nextInt() - 1;
+//            y = scanner.nextInt() - 1;
+//        } while (!isCellValidForMove(x, y));
+//        map[x][y] = DOT_X;
+//        printMap(x, y, COLORS.GREEN);
+//    }
+
     private static void humanTurn() {
         int x, y;
         do {
-            System.out.print("Введите координаты ячейки через пробел: ");
-            x = scanner.nextInt() - 1;
-            y = scanner.nextInt() - 1;
+            System.out.print("Введите координаты свободной ячейки (через пробел): ");
+            x = getValidNumberFromUser() - 1;
+            y = getValidNumberFromUser() - 1;
         } while (!isCellValidForMove(x, y));
         map[x][y] = DOT_X;
+        printMap(x, y, COLORS.GREEN);
+    }
+
+    private static int getValidNumberFromUser() {
+        if (scanner.hasNextInt()) {
+            int n = scanner.nextInt();
+            if (isNumberValid(n)) return n;
+            else System.out.println("Проверьте значение. Допустимый диапазон: 1.." + SIZE);
+        } else {
+            scanner.nextLine();
+            System.out.println("Ввод допускает лишь целые числа");
+        }
+        return -1;
+    }
+
+    private static boolean isNumberValid(int n) {
+        return n >= 1 && n <= SIZE;
     }
 
 
@@ -145,10 +210,7 @@ public class ConsoleGameXO {
      */
     private static void aiTurn() {
         // рейтинг ходов
-        int[][] moveRating = new int[SIZE][SIZE];
-        for (int i = 0; i < moveRating.length; i++)
-            for (int j = 0; j < moveRating.length; j++)
-                moveRating[i][j] = 0;
+        int[][] moveRating = initMoveRating();
         int maxRating = 0;
         int x = -1;
         int y = -1;
@@ -162,10 +224,9 @@ public class ConsoleGameXO {
                 // 2. проверяем может ли этим ходом Ai победить сразу (+20 к рейтингу)
                 map[i][j] = DOT_O;
                 if (checkWin(DOT_O)) {
-                    moveRating[i][j] += 10;
+                    moveRating[i][j] += 20;
                 }
                 map[i][j] = DOT_EMPTY;
-
 
                 // 3. проверяем может ли ход помешать победить Игроку (+10 к рейтингу)
                 //  if (checkCanWin(i, j, DOT_X)) moveRating[i][j] += 10;
@@ -204,22 +265,30 @@ public class ConsoleGameXO {
         printAiTurn(x, y);
     }
 
+    private static int[][] initMoveRating() {
+        int[][] moveRating = new int[SIZE][SIZE];
+        for (int i = 0; i < moveRating.length; i++)
+            Arrays.fill(moveRating[i], 0);
+        return moveRating;
+    }
+
 
     private static void printAiTurn(int x, int y) {
         System.out.println("Компьютер выбрал ячейку: " + (x + 1) + ", " + (y + 1));
         map[x][y] = DOT_O;
+        printMap(x, y, COLORS.RED);
     }
 
 
     public static void showAiMoveRating(int[][] moveRating) {
-        System.out.println("Матрица приоритета ходов");
+        System.out.println(COLORS.GREY + "Матрица приоритета ходов AI:");
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
                 System.out.print(moveRating[i][j] + " ");
             }
             System.out.println();
         }
-        System.out.println("-----------");
+        System.out.println("-----------" + COLORS.SYS);
     }
 
 }
