4a3b789a7d11b761e280005b5939cf479476e428
Богданков Владислав <onlyrus@hotmail.com>
2022-02-11 12:51:31 +0300

    hash

--- src/main/java/com/company/hash/ELinkedHashTable.java
+++ src/main/java/com/company/hash/ELinkedHashTable.java
@@ -1,16 +1,14 @@
 package com.company.hash;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
-public class ELinkedHashTable <K, V> implements IHashTable<K, V> {
+public class ELinkedHashTable<K, V> implements IHashTable<K, V> {
 
     private final LItem<K, V>[] data;
     private int size;
 
-    static class Item<K, V> implements Entry<K, V> {
+    // класс для 1 элемента хранения
+    class Item<K, V> implements Entry<K, V> {
         private final K key;
         private V value;
 
@@ -36,7 +34,7 @@ public class ELinkedHashTable <K, V> implements IHashTable<K, V> {
 
         @Override
         public String toString() {
-            return "{key=" + key +", value=" + value+"}";
+            return "{key " + key + ", value " + value + "}";
         }
 
         @Override
@@ -44,7 +42,7 @@ public class ELinkedHashTable <K, V> implements IHashTable<K, V> {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
             Item<?, ?> item = (Item<?, ?>) o;
-            return Objects.equals(key, item.key) && Objects.equals(value, item.value);
+            return Objects.equals(key, item.key);
         }
 
         @Override
@@ -53,20 +51,34 @@ public class ELinkedHashTable <K, V> implements IHashTable<K, V> {
         }
     }
 
-    class LItem<K, V>  {
+
+    // класс для хранения цепочек (предотвращение коллизий)
+    class LItem<K, V> {
 
         List<Item<K, V>> list;
 
-        public LItem(){
+        public LItem() {
             list = new ArrayList<>();
         }
 
-        public boolean contains(K key, V value){
+        public boolean contains(K key, V value) {
             Item i = new Item<>(key, value);
             return list.contains(i);
         }
 
-        public void add(K key, V value){
+        public boolean contains(K key) {
+            Item i = new Item<>(key, null);
+            return list.contains(i);
+        }
+
+        public V getItemValue(K key) {
+            for (Item<K, V> i : list) {
+                if (i.getKey().equals(key)) return i.getValue();
+            }
+            return null;
+        }
+
+        public void add(K key, V value) {
             list.add(new Item<>(key, value));
         }
 
@@ -74,12 +86,24 @@ public class ELinkedHashTable <K, V> implements IHashTable<K, V> {
         public String toString() {
             if (list == null) return "--";
             else {
-                String st="";
-                for(int i=0;i<list.size();i++){
-                    st+= list.get(i).toString()+", ";
+                String st = "";
+                for (int i = 0; i < list.size(); i++) {
+                    st += list.get(i).toString();
+                    if(i!=list.size()-1) st+=", ";
                 }
                 return st;
             }
+        }
+
+        public V deleteItem(K key) {
+            for(int i=0;i<list.size();i++) {
+                if (list.get(i).getKey().equals(key)) {
+                    V temp = list.get(i).getValue();
+                    list.remove(i);
+                    return temp;
+                }
+            }
+            return null;
         }
     }
 
@@ -92,22 +116,26 @@ public class ELinkedHashTable <K, V> implements IHashTable<K, V> {
         return Math.abs(key.hashCode() % data.length);
     }
 
+    // двойное хеширование (константа д.б. меньше размера массива)
+    private int getStepDoubleHash(K key) {
+        return 3 - (key.hashCode() % 3);
+    }
+
 
     @Override
     public boolean put(K key, V value) {
         int index = hashFunc(key);
 
-        if(data[index]==null) {
+        if (data[index] == null) {
             data[index] = new LItem<>();
             size++;
             data[index].add(key, value);
             return true;
-        }else{
-            if (data[index].contains(key, value)){
-                System.out.println("Элемент"+ new Item<>(key, value).toString()+" уже существует");
+        } else {
+            if (data[index].contains(key, value)) {
+                System.out.println("Элемент с ключом " + key.toString() + " уже существует");
                 return false;
-            }
-            else {
+            } else {
                 size++;
                 data[index].add(key, value);
                 return true;
@@ -115,62 +143,27 @@ public class ELinkedHashTable <K, V> implements IHashTable<K, V> {
         }
     }
 
-    // двойное хеширование (константа д.б. меньше размера массива)
-    private int getStepDoubleHash(K key) {
-        return 5 - (key.hashCode() % 5);
-    }
-
-    // квадратичное пробитие
-    private int getStepQuadro(int n) {
-        return (int) Math.pow(n, 2);
-    }
-
-    // линейное пробитие
-    private int getStepLinear() {
-        return 1;
-    }
-
-    private boolean isKeyEquals(HashTableImpl.Item<K, V> item, K key) {
-        return (item.getKey() == null) ? (key == null) : item.getKey().equals(key);
-    }
-
 
     @Override
     public V get(K key) {
         int index = hashFunc(key);
 
-        if(data[index]==null || (!data[index].contains(key, null))) {
-            System.out.println("Элемент "+key+" не найден");
+        if (data[index] == null || (!data[index].contains(key))) {
+            //System.out.println("Элемент " + key + " не найден");
             return null;
-        }
-        else  return null;
+        } else return data[index].getItemValue(key);
 
-    }
-
-    private int indexOf(K key) {
-//        int index = hashFunc(key);
-//        int count = 0;
-//        while (count++ < data.length) {
-//            if (data[index] == null) break;
-//            if (isKeyEquals(data[index], key)) {
-//                return index;
-//            }
-//            else index+=getStepDoubleHash(key);
-//            index%= data.length;
-//        }
-       return -1;
     }
 
     @Override
     public V remove(K key) {
-//        int index = indexOf(key);
-//        if (index==-1) return null;
-//
-//        HashTableImpl.Item<K, V> removed = data[index];
-//        data[index] = emptyItem;
-//
-//        return removed.getValue();
-        return null;
+        int index = hashFunc(key);
+        if (data[index] == null || (!data[index].contains(key))) {
+            return null;
+        }else {
+            size--;
+            return data[index].deleteItem(key);
+        }
     }
 
     @Override
@@ -180,7 +173,7 @@ public class ELinkedHashTable <K, V> implements IHashTable<K, V> {
 
     @Override
     public boolean isEmpty() {
-        return (size==0);
+        return (size == 0);
     }
 
     @Override
@@ -191,31 +184,41 @@ public class ELinkedHashTable <K, V> implements IHashTable<K, V> {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
+        sb.append("Хеш-таблица:\n");
+        System.out.println("Количество элементов в хеш-таблице = " + size());
         for (int i = 0; i < data.length; i++) {
-            if(data[i]==null) sb.append("---\n");
+            if (data[i] == null) sb.append(i + " = ---\n");
             else
-            sb.append(String.format("%s = %s\n", i, data[i]));
+                sb.append(String.format("%s = %s\n", i, data[i]));
         }
         return sb.toString();
     }
 
     public static void main(String[] args) {
         ELinkedHashTable ht = new ELinkedHashTable(5);
-        ht.put(new Product(1, "Orange"), 150);//1
-        ht.put(new Product(60, "Lemon"), 200);//0
-        ht.put(new Product(70, "Milk"), 250);//0
-        ht.put(new Product(9, "Potato"), 266);//4
+        ht.put(new Product(1, "Orange"), 120);//1
+        ht.put(new Product(60, "Lemon"), 90);//0
+        ht.put(new Product(70, "Milk"), 85);//0
+        ht.put(new Product(8, "Potato"), 77);//3
         ht.put(new Product(4, "Berry"), 452);//4
+        ht.put(new Product(6, "Carrot"), 101);//1
 
-        ht.put(new Product(70, "Milk"), 250);// проверка добавления дубликата
-
-//        System.out.println(ht.get(new Product(60, "Lemon")));
-        System.out.println(ht.get(new Product(62, "Lemon"))); // нет
-        System.out.println(ht.get(new Product(70, "Milk"))); // есть
-
-      //  System.out.println(ht.remove(new Product(60, "Lemon")));
-     //   ht.put(new Product(1000, "Avokado"), 765);//0
-        System.out.println("Size = " + ht.size());
         ht.display();
+
+        System.out.println("Пробуем добавить дубль:");
+        ht.put(new Product(70, "Milk"), 250);// проверка добавления дубликата
+        System.out.println();
+
+        System.out.println("Тестируем поиск:");
+        System.out.println("[62 - Lemon] результат поиска: " + ht.get(new Product(62, "Lemon"))); // нет
+        System.out.println("[70 - Milk] результат поиска: " + ht.get(new Product(70, "Milk"))); // есть
+        System.out.println();
+
+        System.out.println("Тестируем удаление: ");
+        System.out.println("[62 - Lemon] результат удаления: " + ht.remove(new Product(62, "Lemon"))); // нет
+        System.out.println("[70 - Milk] результат удаления: " + ht.remove(new Product(70, "Milk"))); // есть
+
+        ht.display();
+
     }
 }
