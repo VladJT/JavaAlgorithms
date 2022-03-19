806550c68ce1e26642825345f25648130b2d44da
Богданков Владислав <onlyrus@hotmail.com>
2022-03-19 12:57:22 +0300

    Core_11

--- src/main/java/com/company/core/lesson11/PhoneBook.java
+++ src/main/java/com/company/core/lesson11/PhoneBook.java
@@ -1,6 +1,5 @@
 package com.company.core.lesson11;
 
-import java.util.HashMap;
 import java.util.TreeMap;
 
 class PhoneBook {
@@ -10,19 +9,18 @@ class PhoneBook {
         book = new TreeMap<>();
     }
 
-    public void add(String name, String phone) {
+    public void add(String name, String newPhone) {
         if (!book.containsKey(name)) {
-            book.put(name, phone);
-            System.out.println("Добавлена запись: " + name + " " + phone);
+            book.put(name, newPhone);
+            System.out.println("В Справочник добавлена запись: [" + name + " " + newPhone + "]");
         } else {
             String phoneInBook = book.get(name);
-            if (phoneInBook.contains(phone)) {
-                System.out.println("Телефон " + phone + " уже есть в базе у человека " + name);
+            if (phoneInBook.contains(newPhone)) {
+                System.out.println("Телефон [" + newPhone + "] уже присутствует в Справочнике у человека " + name);
             } else {
-                phoneInBook += ", " + phone;
                 book.remove(name);
-                book.put(name, phoneInBook);
-                System.out.println("Добавлена запись: " + name + " " + phoneInBook);
+                book.put(name, phoneInBook + ", " + newPhone);
+                System.out.println("Добавлен телефон [" + newPhone + "] в запись: [" + name + " " + phoneInBook + "]");
             }
         }
     }
@@ -31,27 +29,31 @@ class PhoneBook {
         if (book.containsKey(name)) {
             return book.get(name);
         }
-        return "Нет человека с такой фамилией в базе";
+        return "Нет человека с такой фамилией в Справочнике";
+    }
+
+    public void printBook() {
+        book.forEach((s, s2) -> System.out.printf("%10s:\t %s\n", s, s2));
     }
 
 }
 
 
 class PhoneBookTest {
-
-
     public static void main(String[] args) {
         PhoneBook book = new PhoneBook();
 
         book.add("Иванов", "546-905");
         book.add("Иванов", "+7(911)345-8899");
         book.add("Петров", "123-5567");
-        book.add("Иванов", "546-905");
+        book.add("Иванов", "546-905"); // дубль телефона, не надо добавлять
         book.add("Сидоров", "445-532");
 
-        System.out.println("--------");
+        System.out.println("\n\n---ТЕСТИРУЕМ ПОИСК---");
         System.out.println("Ищем по фамилии Иванов: " + book.get("Иванов"));
-        System.out.println("Ищем по фамилии Сидоров: " + book.get("Сидоров"));
         System.out.println("Ищем по фамилии Васин: " + book.get("Васин"));
+
+        System.out.println("\n\n--ПЕЧАТАЕМ СПРАВОЧНИК--");
+        book.printBook();
     }
 }
