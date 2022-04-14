9a59c3ab5db947e8bba8ef3b749d0361a4214f8f
Владислав Богданков <onlyrus@hotmail.com>
2022-04-14 12:51:04 +0300

    codewars

--- src/main/java/com/company/CodeWars2.java
+++ src/main/java/com/company/CodeWars2.java
@@ -1,130 +1,182 @@
 package com.company;
 
-import javax.imageio.plugins.jpeg.JPEGImageReadParam;
-import javax.swing.plaf.TableHeaderUI;
-import java.util.*;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 class CodeWars2 {
-//Любая живая клетка с менее чем двумя живыми соседями умирает, как если бы это было вызвано недостаточной заселенностью.
-//Любая живая клетка с более чем тремя живыми соседями умирает, как бы от перенаселения.
-//Любая живая клетка с двумя или тремя живыми соседями живет до следующего поколения.
-//Любая мертвая ячейка с ровно тремя живыми соседями становится живой ячейкой.
 
-    public static int[][] getGeneration(int[][] cells, int generations) {
-        for (int i = 0; i < cells.length; i++) {
-            System.out.println(Arrays.toString(cells[i]));
-        }
-        System.out.println("-----> " + generations);
 
-        int[][] map = cells.clone();
-        for (int i = 0; i < generations; i++) {
-            map = getCellMap(map);
-        }
-
-        for (int i = 0; i < map.length; i++) {
-            System.out.println(Arrays.toString(map[i]));
-        }
-        return map;
-    }
-
-    public static int[][] getCellMap(int[][] cells) {
-        int[][] cellMap = new int[cells.length + 2][cells[0].length + 2];
-        for (int i = 0; i < cellMap.length; i++)
-            for (int j = 0; j < cellMap[0].length; j++) {
-                if (i == 0 || j == 0 || i == cellMap.length - 1 || j == cellMap[0].length - 1) {
-                    cellMap[i][j] = 0;
-                } else
-                    cellMap[i][j] = cells[i - 1][j - 1];
+    public static List<Character> escape(char[][] maze) {
+        for (int i = 0; i < maze.length; i++) {
+            for (int j = 0; j < maze[0].length; j++) {
+                System.out.print(maze[i][j]);
             }
+            System.out.println();
+        }
 
-        int[][] newCellMap = new int[cellMap.length][cellMap[0].length];
-        for (int i = 0; i < cellMap.length; i++)
-            for (int j = 0; j < cellMap[0].length; j++) {
-                int nearCellsCount = getNearAliveCellsCount(cellMap, i, j);
-                //alive cell
-                if (cellMap[i][j] == 1) {
-                    if (nearCellsCount == 2 || nearCellsCount == 3) newCellMap[i][j] = 1;
-                    else newCellMap[i][j] = 0;
-                }// dead cell
-                else {
-                    if (nearCellsCount == 3) newCellMap[i][j] = 1;
-                    else newCellMap[i][j] = 0;
+        int x = -1, y = -1;
+        char currentPos = '?';
+        for (int i = 0; i < maze.length; i++) {
+            for (int j = 0; j < maze[0].length; j++) {
+                if (maze[i][j] == 'v' || maze[i][j] == '<' || maze[i][j] == '>' || maze[i][j] == '^') {
+                    x = i;
+                    y = j;
+                    currentPos = maze[i][j];
+                    break;
                 }
             }
-
-        newCellMap = trimMap(newCellMap);
-
-        return newCellMap;
-    }
-
-    private static int[][] trimMap(int[][] newCellMap) {
-        int startRow = -1, endRow = -1;
-        int startCol = -1, endCol = -1;
-        for (int i = 0; i < newCellMap.length; i++) {
-            int sumRow = Arrays.stream(newCellMap[i]).sum();
-            if (sumRow > 0) {
-                if (startRow == -1) startRow = i;
-                endRow = i;
-            }
         }
 
-        for (int j = 0; j < newCellMap[0].length; j++) {
-            int sumCol = 0;
-            for (int i = 0; i < newCellMap.length; i++) {
-                sumCol += newCellMap[i][j];
+        bestWay = "";
+        findWay(maze, x, y, "");
+
+        System.out.println("RESULT : " + bestWay);
+
+        List<Character> resultWay = new ArrayList<>();
+        if (bestWay.equals("")) return resultWay;
+
+        String[] st = bestWay.split(";\\s");
+        int oldX, oldY;
+        for (int i = 1; i < st.length; i++) {
+            oldX = Integer.parseInt(st[i-1].split(":")[0]);
+            oldY = Integer.parseInt(st[i-1].split(":")[1]);
+            x = Integer.parseInt(st[i].split(":")[0]);
+            y = Integer.parseInt(st[i].split(":")[1]);
+
+            // move down
+            if(x-oldX == 1){
+                if(currentPos=='<') resultWay.add('L');
+                if(currentPos=='>') resultWay.add('R');
+                if(currentPos=='^') resultWay.add('B');
+                currentPos = 'v';
+            }// move up
+            if(x-oldX == -1){
+                if(currentPos=='<') resultWay.add('R');
+                if(currentPos=='>') resultWay.add('L');
+                if(currentPos=='v') resultWay.add('B');
+                currentPos = '^';
+            }// move right
+            if(y-oldY == 1){
+                if(currentPos=='<') resultWay.add('B');
+                if(currentPos=='^') resultWay.add('R');
+                if(currentPos=='v') resultWay.add('L');
+                currentPos = '>';
+            }// move left
+            if(y-oldY == -1){
+                if(currentPos=='>') resultWay.add('B');
+                if(currentPos=='^') resultWay.add('L');
+                if(currentPos=='v') resultWay.add('R');
+                currentPos = '<';
             }
-            if (sumCol > 0) {
-                if (startCol == -1) startCol = j;
-                endCol = j;
-            }
+            resultWay.add('F');
         }
 
-        if (startRow == -1 && endRow == -1 && startCol == -1 && endCol == -1) {
-            return new int[0][0];
+        return resultWay;
+    }
+
+    static String bestWay = "";
+
+    private static void findWay(char[][] maze, int x, int y, String way) {
+        if (!bestWay.equals("")) return;// has best way!
+
+        maze[x][y] = '.';
+        way += x + ":" + y + "; ";
+
+        if (x == 0 || x == maze.length - 1 || y == 0 || y == maze[0].length - 1) {
+            // find exit!!
+            System.out.println("find!!! " + way);
+            bestWay = way;
+            return;
         }
 
-        int[][] newMap = new int[endRow - startRow + 1][endCol - startCol + 1];
-        for (int i = 0; i < newMap.length; i++)
-            for (int j = 0; j < newMap[0].length; j++)
-                newMap[i][j] = newCellMap[i + startRow][j + startCol];
-        return newMap;
+        if (existsWay(x - 1, y, maze)) {
+            findWay(maze, x - 1, y, way);
+        }
+        if (existsWay(x + 1, y, maze)) {
+            findWay(maze, x + 1, y, way);
+        }
+        if (existsWay(x, y - 1, maze)) {
+            findWay(maze, x, y - 1, way);
+        }
+        if (existsWay(x, y + 1, maze)) {
+            findWay(maze, x, y + 1, way);
+        }
+        //      System.out.println("bad way!! " + way);
     }
 
-    private static int getNearAliveCellsCount(int[][] cellMap, int i, int j) {
-        int count = 0;
-        for (int x = i - 1; x <= i + 1; x++)
-            for (int y = j - 1; y <= j + 1; y++) {
-                if (x == i && y == j) continue;//current cell not count
+    private static String findWay2(char[][] maze, int x, int y) {
+        String way = "";
+        maze[x][y] = '.';
+        way = x + ":" + y + "; ";
 
-                if (x >= 0 && x < cellMap.length && y >= 0 && y < cellMap[0].length)
-                    count += cellMap[x][y];
-            }
-        return count;
+
+        if (existsWay(x - 1, y, maze)) {
+            way += findWay2(maze, x - 1, y);
+        }
+        if (existsWay(x + 1, y, maze)) {
+            way += findWay2(maze, x + 1, y);
+        }
+        if (existsWay(x, y - 1, maze)) {
+            way += findWay2(maze, x, y - 1);
+        }
+        if (existsWay(x, y + 1, maze)) {
+            way += findWay2(maze, x, y + 1);
+        }
+
+        if (x == 0 || x == maze.length - 1 || y == 0 || y == maze[0].length - 1) {
+            // find exit!!
+            System.out.println("find!!! " + way);
+            return way;
+        }
+        return way;
     }
+
+    private static boolean existsWay(int x, int y, char[][] maze) {
+        if (x >= 0 && x < maze.length && y >= 0 && y < maze[0].length && maze[x][y] == ' ') {
+            return true;
+        }
+        return false;
+    }
+
 
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
         //-------------------------
-        int[][][] gliders = {
-                {{1, 0, 0},
-                        {0, 1, 1},
-                        {1, 1, 0}},
-                {{0, 1, 0},
-                        {0, 0, 1},
-                        {1, 1, 1}}
-        };
 
-      //  int[][] res = getGeneration(gliders[0], 1);
-        int[][] arr = {{1, 1, 1, 0, 0, 0, 1, 0},
-                       {1, 0, 0, 0, 0, 0, 0, 1},
-                       {0, 1, 0, 0, 0, 1, 1, 1}};
-        int[][] rez = getGeneration(arr, 16);
+        List<char[][]> basicMazes = new ArrayList<>();
+        basicMazes.add(new char[][] {
+                "# #".toCharArray(),
+                " > ".toCharArray(),
+                "# #".toCharArray()
+        });
+        basicMazes.add(new char[][]{
+                "####### #".toCharArray(),
+                "#>#   # #".toCharArray(),
+                "#   #   #".toCharArray(),
+                "#########".toCharArray()
+        });
+        basicMazes.add(new char[][]{
+                "###########".toCharArray(),
+                "#        >#".toCharArray(),
+                "###########".toCharArray()
+        });
+        basicMazes.add(new char[][]{
+                "##########".toCharArray(),
+                "#        #".toCharArray(),
+                "#  ##### #".toCharArray(),
+                "#  #   # #".toCharArray(),
+                "#  #^# # #".toCharArray(),
+                "#  ### # #".toCharArray(),
+                "#      # #".toCharArray(),
+                "######## #".toCharArray()
+        });
+
+        System.out.println(escape(basicMazes.get(0)));
+        System.out.println(escape(basicMazes.get(1)));
+        System.out.println(escape(basicMazes.get(2)));
+        System.out.println(escape(basicMazes.get(3)));
 
         //-------------------------
         System.out.println("Время выполнения (милисек.): " + (System.currentTimeMillis() - startTime));
